c3749e2ddf2171513e06265d9b85ba7000da8974
lorraine98 <hyosung0425@naver.com>
2021-08-24 05:52:22 +0900

    add clock, time func

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import DateTime from "./Datetime.js";
 
 export default function App({ $target }) {
+  this.$target = $target;
 
- 
+  this.render = () => {
+    this.$target.innerHTML = `<div class="date-time fontXl font600"></div>`;
+  };
 
+  this.render();
 
-
+  new DateTime({
+    $target: this.$target.querySelector(".date-time"),
+  });
 }
--- src/Datetime.js
+++ src/Datetime.js
@@ -1,25 +1,10 @@
-function convertToZeroPad(digit) {
-    return `${digit}`.padStart(2, '0');
-}
+export default function DateTime({ $target }) {
+  this.$target = $target;
 
-function getCurrentDateTime() {
-    const today = new Date();
-    const currentDateTime = {
-        year: today.getFullYear(),
-        month: new Intl.DateTimeFormat('en-US', { month: 'short' }).format(today),
-        date: convertToZeroPad(today.getDate()),
-        day: new Intl.DateTimeFormat('en-US', { weekday: 'short' }).format(today)
-    }
-    return currentDateTime
-}
+  this.render = () => {
+    const { year, month, date, day, hours, minutes } = getCurrentDateTime();
 
-
-setInterval(function () {
-    const curDateTime = getCurrentDateTime();
-    // console.log(curDateTime);
-}, 1000);
-
-const dateTimeHTML = `
+    this.$target.innerHTML = `
     <div class="date">
         <span>${date}</span>
         <div class="date__middle">
@@ -28,10 +13,29 @@ const dateTimeHTML = `
         </div>
         <span>${day}</span>
     </div>
-    <div class="time">03:52 PM</div>
-`
+    <div class="time">${hours}:${minutes} ${hours > 12 ? "PM" : "AM"}</div>
+`;
+  };
+  this.render();
 
-export function renderDateTime() {
-    const $dateTime = document.querySelector('.date_time');
-    $dateTime.innerHTML = dateTimeHTML;
+  setInterval(() => {
+    this.render();
+  }, 1000);
+}
+
+function convertToZeroPad(digit) {
+  return `${digit}`.padStart(2, "0");
+}
+
+function getCurrentDateTime() {
+  const today = new Date();
+  const currentDateTime = {
+    year: today.getFullYear(),
+    month: new Intl.DateTimeFormat("en-US", { month: "short" }).format(today),
+    date: convertToZeroPad(today.getDate()),
+    day: new Intl.DateTimeFormat("en-US", { weekday: "short" }).format(today),
+    hours: today.getHours(),
+    minutes: today.getMinutes(),
+  };
+  return currentDateTime;
 }
